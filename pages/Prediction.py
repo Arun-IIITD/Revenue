23eff5452ffca93758c74e9f8a6c6e3a78a264a7
Annu117 <108427028+Annu117@users.noreply.github.com>
2023-12-13 00:30:54 +0530

    Add files via upload

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -12,8 +12,13 @@ from prophet.plot import add_changepoints_to_plot
 import altair as alt  
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from streamlit_extras.switch_page_button import switch_page
+from statistics import mean
+from io import BytesIO
+import plotly.graph_objects as go
+import base64
 
 st.set_page_config(page_title="Revenue Forecasting", page_icon=":overview", layout="wide", initial_sidebar_state="collapsed")
+
 
 def set_custom_styles():
     """
@@ -61,7 +66,6 @@ def custom_top_bar(selected_page=None):
     Custom HTML for a fixed top bar.
     """
     selected_page = selected_page or "Prediction"
-    # current_file = __file__.split("/")[-1]
     current_file = os.path.basename(__file__)
     custom_top_bar = f"""
     <style>
@@ -126,147 +130,249 @@ selected_page = url_to_page.get(url_path)
 custom_top_bar(selected_page)
 # -----------------------------------------------
 
+Actual_for_ALL_days = []
+Predicted_for_ALL_days = []
+
 connection_uri = "mongodb+srv://annu21312:6dPsrXPfhm19YxXl@hello.hes3iy5.mongodb.net/"
 client = pymongo.MongoClient(connection_uri, serverSelectionTimeoutMS=30000)
 database_name = "Revenue_Forecasting"
-db = client[database_name]
+db = client[database_name] 
+collection5 = db["accuracy_revenue"]
+cursor5 = collection5.find({})
+data5 = pd.DataFrame(list(cursor5))
+train_data = data5.iloc[:760]
+test_data_for_next_7_days = data5.iloc[760:767]
+test_data_for_next_14_days = data5.iloc[767:774]
+test_data_for_next_21_days = data5.iloc[774:781]
 
-# Fetch data from MongoDB
-collection4 = db["Prophet"]
-cursor2 = collection4.find({})
-data = pd.DataFrame(list(cursor2))
 
-data.rename(columns={'Date': 'ds', 'Revenue': 'y'}, inplace=True)
+#FOR 1st 7 DAYS(1-7)
+model = Prophet(
+                        changepoint_prior_scale= 0.3,
+                        #holidays_prior_scale = 0.8,
+                        n_changepoints = 500,
+                        seasonality_mode = 'multiplicative',
+                        weekly_seasonality=True,
+                        daily_seasonality = True,
+                        yearly_seasonality = True,
+                        interval_width=0.8)
+model.fit(train_data)
+future_for_7_days = model.make_future_dataframe(periods=7, freq='D', include_history=False)
+forecast = model.predict(future_for_7_days)
+next_7_days = forecast.tail(7)
+Actual_for_7_days =  []
+Predicted_for_7_days = []
+Accuracy_for_7_days = []
+for i,j in zip(list(test_data_for_next_7_days['y'].tail(7)),list(next_7_days['yhat'])):
+    i= int(i)
+    j = int(j)
+    Actual_for_7_days.append(i)
+    Predicted_for_7_days.append(j)
 
-# Initialize and fit the Prophet model
-model = Prophet()
-model.fit(data)
+for i,j in zip(Actual_for_7_days,Predicted_for_7_days):
+    c = abs(i-j)
+    c = c*100/i
+    c  = 100-c
+    c= int(c)
+    Accuracy_for_7_days.append(c)
 
-# Create a future DataFrame for forecasting (forecast for the next 7 days)
-future = model.make_future_dataframe(periods=7)
+#FOR next 7 DAYS (8-14)
+model1 = Prophet(
+                      changepoint_prior_scale= 0.9,
+                      holidays_prior_scale = 0.4,
+                      n_changepoints = 400,
+                      seasonality_mode = 'additive',
+                      weekly_seasonality=True,
+                      daily_seasonality = True,
+                      yearly_seasonality = True,
+                      interval_width=0.5)
+model1.fit(train_data)
+future_for_14_days = model1.make_future_dataframe(periods=14, freq='D', include_history=False)
+forecast1 = model1.predict(future_for_14_days)
+next_14_days = forecast1.tail(7)
+Actual_for_14_days =  []
+Predicted_for_14_days = []
+Accuracy_for_14_days = []
+for i,j in zip(list(test_data_for_next_14_days['y'].tail(7)),list(next_14_days['yhat'])):
+   
+    i= int(i)
+    j = int(j)
+    Actual_for_14_days.append(i)
+    Predicted_for_14_days.append(j)
 
-# Generate forecasts
-forecast = model.predict(future)
+for i,j in zip(Actual_for_14_days,Predicted_for_14_days):
+    c = abs(i-j)
+    c = c*100/i
+    c  = 100-c
+    c= int(c)
+    Accuracy_for_14_days.append(c)
 
-# ACTUAL REVENUE FOR 7 DAYS
-actual_data = data.tail(7)
+# For the next 7 days(15-21 days)
+model2 = Prophet(
+                      changepoint_prior_scale= 0.1,
+                      holidays_prior_scale = 0.4,
+                      n_changepoints = 200,
+                      seasonality_mode = 'multiplicative',
+                      weekly_seasonality=True,
+                      daily_seasonality = True,
+                      yearly_seasonality = True,
+                      interval_width=0.9)
+model2.fit(train_data)
+future_for_21_days = model2.make_future_dataframe(periods=21, freq='D', include_history=False)
+forecast2 = model2.predict(future_for_21_days)
+next_21_days = forecast2.tail(7)
+Actual_for_21_days =  []
+Predicted_for_21_days = []
+Accuracy_for_21_days = []
+for i,j in zip(list(test_data_for_next_21_days['y'].tail(7)),list(next_21_days['yhat'])):
+    i= int(i)
+    j = int(j)
+    Actual_for_21_days.append(i)
+    Predicted_for_21_days.append(j)
 
-# PREDICTIONS FOR 7 DAYS
-next_7_days_forecast = forecast.tail(7)
+for i,j in zip(Actual_for_21_days,Predicted_for_21_days):
+    c = abs(i-j)
+    c = c*100/i
+    c  = 100-c
+    c= int(c)
+    Accuracy_for_21_days.append(c)
 
-predictions_for_7_days = [int(x) for x in list(next_7_days_forecast['yhat'])]
-actual_data_for_7_days = [int(x) for x in list(actual_data['y'])]
+def plot_revenue(actual_dates, actual_revenue, predicted_dates, predicted_revenue, title):
+    fig = go.Figure()
 
-# Calculate accuracy as a percentage
-accuracy = np.mean([pred / actual * 100 for pred, actual in zip(next_7_days_forecast['yhat'], actual_data['y'])])
-# Calculate accuracy metrics
-mae = mean_absolute_error(actual_data['y'], next_7_days_forecast['yhat'])
-mse = mean_squared_error(actual_data['y'], next_7_days_forecast['yhat'])
-rmse = np.sqrt(mse)
-st.markdown("\n")
-st.markdown("\n")
-st.markdown("<div class='section-title' style='font-weight: bold; font-size: 24px;'>Forecasting for next week</div>", unsafe_allow_html=True)
-
-# Data Presentation
-col1, col2=st.columns(2)
-with col1:
-    st.markdown('Prediction')
-    st.dataframe(pd.DataFrame({'Date': next_7_days_forecast['ds'], 'Predicted Revenue': predictions_for_7_days}))
-with col2:
-    st.markdown('Actual Revenue')
-    st.dataframe(pd.DataFrame({'Date': actual_data['ds'], 'Actual Revenue': actual_data_for_7_days}))
-
-# CSS for styling
-st.markdown(
-    """
-    <style>
-    /* Adjust text size */
-    .big-text {
-        font-size: 20px;
-    }
+    # Plot actual revenue
+    fig.add_trace(go.Scatter(x=actual_dates, y=actual_revenue, mode='lines+markers', name='Actual Revenue', line=dict(color='blue'), textposition='bottom center'))
     
-    .small-text {
-        font-size: 14px; /* Adjust the font size as needed */
-    }
+    # Plot predicted revenue
+    fig.add_trace(go.Scatter(x=predicted_dates, y=predicted_revenue, mode='lines+markers', name='Predicted Revenue', line=dict(color='red'), textposition='bottom center'))
 
-    /* Style the table */
-    table.dataframe {
-        font-size: 16px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-# st.subheader(f'Accuracy: {accuracy:.2f}%')
-st.markdown(f'Accuracy: {accuracy:.2f}%')
-# st.markdown("<div class='section-title'>Revenue Forecast Plot</div>", unsafe_allow_html=True)
-# st.header('Revenue Forecast Plot (Altair Chart):')
-forecast_chart = alt.Chart(forecast).mark_line().encode(
-    x=alt.X('ds:T', title='Date', axis=alt.Axis(labelFontSize=12, titleFontSize=14)),
-    y=alt.Y('yhat:Q', title='Revenue Prediction', axis=alt.Axis(labelFontSize=12, titleFontSize=14)),
-    color=alt.value('blue'),  # Line color
-    strokeWidth=alt.value(2),  # Line width
-).properties(
-    width=700,  # Chart width
-    height=400,  # Chart height
-    title='Revenue Forecast',  # Chart title
-).configure_title(
-    fontSize=16,  # Title font size
-    anchor='start'  # Title alignment
-).configure_axis(
-    labelFontSize=12,  # Axis label font size
-    titleFontSize=14  # Axis title font size
-)
-st.markdown(
-    """
-    <style>
-    .altair-chart {
-        width: 100%;
-        height: 400px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-st.altair_chart(forecast_chart, use_container_width=True)
-# st.altair_chart(forecast_chart, use_container_width=True, key='forecast_chart')
-
-# -------------------------------
-
-col3, col4=st.columns(2)
-with col3:
-    st.markdown("<div class='section-title'>Revenue Components</div>", unsafe_allow_html=True)
-    # Altair Chart for components that closely resembles the Matplotlib plot
-    components_chart = alt.Chart(forecast).mark_area().encode(
-        x='ds:T',
-        y='trend:Q',
-        y2='yhat_upper:Q',
-        opacity=alt.value(0.5)
-    ).interactive()
-
-    st.altair_chart(components_chart, use_container_width=True)
-    # st.altair_chart(components_chart, use_container_width=True, key='components_chart')
-
-# ------------------------------
-with col4:
-    trend_chart = alt.Chart(forecast).mark_line().encode(
-        x=alt.X('ds:T', title='Date', axis=alt.Axis(labelFontSize=12, titleFontSize=14)),
-        y=alt.Y('trend:Q', title='Trend', axis=alt.Axis(labelFontSize=12, titleFontSize=14)),
-        color=alt.value('blue'),  # Line color
-        strokeWidth=alt.value(2),  # Line width
-    ).properties(
-        width=700,  # Chart width
-        height=400,  # Chart height
-        title='Trend Component',  # Chart title
-    ).configure_title(
-        fontSize=16,  # Title font size
-        anchor='start'  # Title alignment
-    ).configure_axis(
-        labelFontSize=12,  # Axis label font size
-        titleFontSize=14  # Axis title font size
+    # Update layout for better interactivity
+    fig.update_layout(
+        title=title,
+        xaxis=dict(title='Date'),
+        yaxis=dict(title='Revenue (in Lakhs)'),
+        hovermode='x',
+        showlegend=False,  # Hide legend for cleaner appearance
+        legend_title='Legend',
+        font=dict(family='Arial', size=14),
+        shapes=[
+            dict(
+                type='line',
+                x0=0,
+                x1=1,
+                y0=0,
+                y1=0,
+                line=dict(color='lightgray', width=0.5),
+                xref='paper',
+                yref='paper'
+            ),
+            dict(
+                type='line',
+                x0=0,
+                x1=0,
+                y0=0,
+                y1=1,
+                line=dict(color='lightgray', width=0.5),
+                xref='paper',
+                yref='paper'
+            ),
+            dict(
+                type='line',
+                x0=1,
+                x1=1,
+                y0=0,
+                y1=1,
+                line=dict(color='lightgray', width=0.5),
+                xref='paper',
+                yref='paper'
+            ),
+            dict(
+                type='line',
+                x0=0,
+                x1=1,
+                y0=1,
+                y1=1,
+                line=dict(color='lightgray', width=0.5),
+                xref='paper',
+                yref='paper'
+            ),
+        ],
+        height=400,  # Adjust the height of the plot
+        width=530,   # Adjust the width of the plot
+        margin=dict(l=20, r=20, t=40, b=20),
     )
-    st.altair_chart(trend_chart, use_container_width=True)
-    # st.altair_chart(trend_chart, use_container_width=True, key='trend_chart')
 
-# with col1:
+    # Show the plot
+    st.plotly_chart(fig)
+# ---------------------------------------------------------------------
+def main():
+    st.subheader('Revenue Prophet')
+    col1, col2 = st.columns(2)
+    
+    # Plot for 0-7 Days
+    with col1:
+        df_7_days = pd.DataFrame({'Date': test_data_for_next_7_days['ds'], 'Actual': Actual_for_7_days, 'Predicted': Predicted_for_7_days})
+        # fig_7_days = px.line(df_7_days, x='Date', y=['Actual', 'Predicted'], title='Predicted vs Actual Revenue for 0-7 Days')
+        plot_revenue(df_7_days['Date'], df_7_days['Actual'], df_7_days['Date'], df_7_days['Predicted'], 'Predicted vs Actual Revenue for 0-7 Days')
+        # st.plotly_chart(fig_7_days)
+        st.write(f"Accuracy: {round(mean(Accuracy_for_7_days))}%")
+        st.markdown("---")
+        #Conversion of dfs into excel files
+        revenue_df_7_days = pd.DataFrame({
+            'Date': test_data_for_next_7_days['ds'].tail(7),
+            'Actual_Revenue': Actual_for_7_days,
+            'Predicted_Revenue': Predicted_for_7_days,
+            'Accuracy_of_revenue': Accuracy_for_7_days,
+        })
+    # Plot for 8-14 Days
+    with col1:
+        df_14_days = pd.DataFrame({'Date': next_14_days['ds'].tail(7), 'Actual': Actual_for_14_days, 'Predicted': Predicted_for_14_days})
+        # fig_14_days = px.line(df_14_days, x='Date', y=['Actual', 'Predicted'], title='Predicted vs Actual Revenue for 8-14 Days')
+        # st.plotly_chart(fig_14_days)
+        plot_revenue(df_14_days['Date'], df_14_days['Actual'], df_14_days['Date'], df_14_days['Predicted'], 'Predicted vs Actual Revenue for 8-14 Days')
+        st.write(f"Accuracy: {round(mean(Accuracy_for_14_days))}%")
+        st.markdown("---")
+        revenue_df_14_days = pd.DataFrame({
+            'Date': next_14_days['ds'].tail(7),
+            'Actual_Revenue': Actual_for_14_days,
+            'Predicted_Revenue': Predicted_for_14_days,
+            'Accuracy_of_revenue': Accuracy_for_14_days,
+        })
+    # Plot for 15-21 Days
+    with col2:
+        df_21_days = pd.DataFrame({'Date': next_21_days['ds'].tail(7), 'Actual': Actual_for_21_days, 'Predicted': Predicted_for_21_days})
+        # fig_21_days = px.line(df_21_days, x='Date', y=['Actual', 'Predicted'], title='Predicted vs Actual Revenue for 15-21 Days')
+        # st.plotly_chart(fig_21_days)
+        plot_revenue(df_21_days['Date'], df_21_days['Actual'], df_21_days['Date'], df_21_days['Predicted'], 'Predicted vs Actual Revenue for 15-21 Days')
+
+        st.write(f"Accuracy: {round(mean(Accuracy_for_21_days))}%")
+        st.markdown("---")
+        revenue_df_21_days = pd.DataFrame({
+            'Date': next_21_days['ds'].tail(7),
+            'Actual_Revenue': Actual_for_21_days,
+            'Predicted_Revenue': Predicted_for_21_days,
+            'Accuracy_of_revenue': Accuracy_for_21_days,
+        })
+    st.subheader('Download Excel Files')
+
+    # Download links for Excel files
+    with BytesIO() as buffer:
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            revenue_df_7_days.to_excel(writer, sheet_name='revenue_data_7_days', index=False)
+            revenue_df_14_days.to_excel(writer, sheet_name='revenue_data_14_days', index=False)
+            revenue_df_21_days.to_excel(writer, sheet_name='revenue_data_21_days', index=False)
+        buffer.seek(0)
+
+        st.markdown(get_excel_download_link(buffer, 'revenue_data_7_days.xlsx', 'Download revenue_data_7_days.xlsx'), unsafe_allow_html=True)
+        st.markdown(get_excel_download_link(buffer, 'revenue_data_14_days.xlsx', 'Download revenue_data_14_days.xlsx'), unsafe_allow_html=True)
+        st.markdown(get_excel_download_link(buffer, 'revenue_data_21_days.xlsx', 'Download revenue_data_21_days.xlsx'), unsafe_allow_html=True)
+
+# Function to generate a download link for the Excel file
+def get_excel_download_link(buffer, file_name, link_text):
+    buffer = buffer.read()
+    b64 = base64.b64encode(buffer).decode()
+    href = f'<a href="data:application/octet-stream;base64,{b64}" download="{file_name}">{link_text}</a>'
+    return href
+
+if __name__ == '__main__':
+    main()

5f7ab656d0125297f4bd2dd5ec8328f218b27aeb
Arun23297 <136068289+Arun-IIITD@users.noreply.github.com>
2024-01-08 18:08:27 +0530

    Update Prediction.py

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -420,7 +420,7 @@ def main():
         plot_revenue(df_7_days['Date'], df_7_days['Actual'], df_7_days['Date'], df_7_days['Predicted'], 'For 0-07 Days')
         # st.plotly_chart(fig_7_days)
         st.write(f"Accuracy: {round(mean(Accuracy_for_7_days))}%")
-        st.write(f"Sensitivity: {(mean(sensitivity_values_for_7_days))}%")
+        st.write(f"Sensitivity: {round(mean(sensitivity_values_for_7_days))}")
         
         
         st.markdown("---")
@@ -438,7 +438,7 @@ def main():
         # st.plotly_chart(fig_14_days)
         plot_revenue(df_14_days['Date'], df_14_days['Actual'], df_14_days['Date'], df_14_days['Predicted'], 'For 8-14 Days')
         st.write(f"Accuracy: {round(mean(Accuracy_for_14_days))}%")
-        st.write(f"Sensitivity: {round(mean(sensitivity_values_for_14_days))}%")
+        st.write(f"Sensitivity: {round(mean(sensitivity_values_for_14_days))}")
         st.markdown("---")
         revenue_df_14_days = pd.DataFrame({
             'Date': next_14_days['ds'].tail(7),
@@ -454,7 +454,7 @@ def main():
         plot_revenue(df_21_days['Date'], df_21_days['Actual'], df_21_days['Date'], df_21_days['Predicted'], 'For 15-21 Days')
 
         st.write(f"Accuracy: {round(mean(Accuracy_for_21_days))}%")
-        st.write(f"Sensitivity: {(mean(sensitivity_values_for_21_days))}%")
+        st.write(f"Sensitivity: {round(mean(sensitivity_values_for_21_days))}")
         st.markdown("---")
         revenue_df_21_days = pd.DataFrame({
             'Date': next_21_days['ds'].tail(7),

355b5e6a1fcc3fd08aeba771adfdd02dd1148b3e
Arun23297 <136068289+Arun-IIITD@users.noreply.github.com>
2024-02-22 18:01:07 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+onnection_uri = "mongodb+srv://annu21312:6dPsrXPfhm19YxXl@hello.hes3iy5.mongodb.net/"
+# client = pymongo.MongoClient(connection_uri, serverSelectionTimeoutMS=30000)
+# database_name = "Revenue_Forecasting"
+
+# db = client[database_name]
+# collection = db["Forecasting"]
+# #cursor = collection.find({})
+# data_to_insert = df.to_dict(orient='records')
+# #collection = db["collection"]
+# result = collection.insert_many(data_to_insert)
+# print("Inserted document IDs:", result.inserted_ids)
+# client.close()

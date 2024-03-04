69ef9bb5b7095131efc21e99c55d2c89b222564c
Arun-IIITD <136068289+Arun-IIITD@users.noreply.github.com>
2024-03-04 17:00:31 +0530

    Update View.py

--- pages/View.py
+++ pages/View.py
@@ -193,65 +193,65 @@ def main():
     #---------------------------------------------------------------------------
    #YEARLY, MONTH, DAILY AND WEEKLY VIEW
     st.subheader("VIEW")
-    data4 = data4.drop_duplicates() 
-    data4['Business Date'] = pd.to_datetime(data4['Business Date'])
-    data4['Month_Year'] = data4['Business Date'].dt.to_period('M')
-    data4_monthly = data4.groupby('Month_Year').sum()  
-    data4_monthly.index = data4_monthly.index.to_timestamp()
-    data4_monthly['Month_Year'] = data4_monthly.index.strftime('%B_%Y')
-    data4['Business Date'] = pd.to_datetime(data4['Business Date'])
-    data4['Business Date'] = data4['Business Date'].dt.date
-    data4['Business Date'] = pd.to_datetime(data4['Business Date'])
-    data4['Year'] = data4['Business Date'].dt.to_period('Y')
-    data4_yearly = data4.groupby('Year').sum()
-    data4_yearly['Year'] = data4_yearly.index.astype(str)
-    data4['Week'] = data4['Business Date'].dt.to_period('W')
-    data4_weekly = data4.groupby('Week').sum()
-    data4_weekly.index = data4_weekly.index.to_timestamp()
-    data4_weekly['Week'] = data4_weekly.index.strftime('%U_%Y')
+    # data4 = data4.drop_duplicates() 
+    # data4['Business Date'] = pd.to_datetime(data4['Business Date'])
+    # data4['Month_Year'] = data4['Business Date'].dt.to_period('M')
+    # data4_monthly = data4.groupby('Month_Year').sum()  
+    # data4_monthly.index = data4_monthly.index.to_timestamp()
+    # data4_monthly['Month_Year'] = data4_monthly.index.strftime('%B_%Y')
+    # data4['Business Date'] = pd.to_datetime(data4['Business Date'])
+    # data4['Business Date'] = data4['Business Date'].dt.date
+    # data4['Business Date'] = pd.to_datetime(data4['Business Date'])
+    # data4['Year'] = data4['Business Date'].dt.to_period('Y')
+    # data4_yearly = data4.groupby('Year').sum()
+    # data4_yearly['Year'] = data4_yearly.index.astype(str)
+    # data4['Week'] = data4['Business Date'].dt.to_period('W')
+    # data4_weekly = data4.groupby('Week').sum()
+    # data4_weekly.index = data4_weekly.index.to_timestamp()
+    # data4_weekly['Week'] = data4_weekly.index.strftime('%U_%Y')
     
-    data5 = data5.drop_duplicates() 
-    data5['Business Date'] = pd.to_datetime(data5['Business Date'])
-    data5['Month_Year'] = data5['Business Date'].dt.to_period('M')
-    data5_monthly = data5.groupby('Month_Year').sum()  
-    data5_monthly.index = data5_monthly.index.to_timestamp()
-    data5_monthly['Month_Year'] = data5_monthly.index.strftime('%B_%Y')
-    data5['Business Date'] = pd.to_datetime(data5['Business Date'])
-    data5['Business Date'] = data5['Business Date'].dt.date
-    data5['Business Date'] = pd.to_datetime(data5['Business Date'])
-    data5['Year'] = data5['Business Date'].dt.to_period('Y')
-    data5_yearly = data5.groupby('Year').sum()
-    data5_yearly['Year'] = data5_yearly.index.astype(str)
-    data5['Week'] = data5['Business Date'].dt.to_period('W')
-    data5_weekly = data5.groupby('Week').sum()
-    data5_weekly.index = data5_weekly.index.to_timestamp()
-    data5_weekly['Week'] = data5_weekly.index.strftime('%U_%Y')
+    # data5 = data5.drop_duplicates() 
+    # data5['Business Date'] = pd.to_datetime(data5['Business Date'])
+    # data5['Month_Year'] = data5['Business Date'].dt.to_period('M')
+    # data5_monthly = data5.groupby('Month_Year').sum()  
+    # data5_monthly.index = data5_monthly.index.to_timestamp()
+    # data5_monthly['Month_Year'] = data5_monthly.index.strftime('%B_%Y')
+    # data5['Business Date'] = pd.to_datetime(data5['Business Date'])
+    # data5['Business Date'] = data5['Business Date'].dt.date
+    # data5['Business Date'] = pd.to_datetime(data5['Business Date'])
+    # data5['Year'] = data5['Business Date'].dt.to_period('Y')
+    # data5_yearly = data5.groupby('Year').sum()
+    # data5_yearly['Year'] = data5_yearly.index.astype(str)
+    # data5['Week'] = data5['Business Date'].dt.to_period('W')
+    # data5_weekly = data5.groupby('Week').sum()
+    # data5_weekly.index = data5_weekly.index.to_timestamp()
+    # data5_weekly['Week'] = data5_weekly.index.strftime('%U_%Y')
 
-    view_option = st.selectbox("Select View", ['Yearly', 'Monthly','Weekly','Daily'])
+    # view_option = st.selectbox("Select View", ['Yearly', 'Monthly','Weekly','Daily'])
 
-    if view_option == 'Daily':
-        st.write("Daily View")
-        st.write("Data from Sep2021 - Dec2021 is zero due to covid.")
-        st.dataframe(data4[['Business Date', 'Room Revenue','Rooms Sold']])  
-        st.dataframe(data5[['Business Date','Arrival Rooms','Individual Confirm','Individual Revenue']])  
+    # if view_option == 'Daily':
+    #     st.write("Daily View")
+    #     st.write("Data from Sep2021 - Dec2021 is zero due to covid.")
+    #     st.dataframe(data4[['Business Date', 'Room Revenue','Rooms Sold']])  
+    #     st.dataframe(data5[['Business Date','Arrival Rooms','Individual Confirm','Individual Revenue']])  
 
-    elif view_option == 'Monthly':
-        st.write("Monthly View")
-        st.write("Data from Sep2021 - Dec2021 is zero due to covid.")
-        st.dataframe(data4_monthly.reset_index(drop=True)[['Month_Year', 'Room Revenue','Rooms Sold']])  
-        st.dataframe(data5_monthly.reset_index(drop=True)[['Month_Year','Arrival Rooms','Individual Confirm','Individual Revenue']])  
+    # elif view_option == 'Monthly':
+    #     st.write("Monthly View")
+    #     st.write("Data from Sep2021 - Dec2021 is zero due to covid.")
+    #     st.dataframe(data4_monthly.reset_index(drop=True)[['Month_Year', 'Room Revenue','Rooms Sold']])  
+    #     st.dataframe(data5_monthly.reset_index(drop=True)[['Month_Year','Arrival Rooms','Individual Confirm','Individual Revenue']])  
 
-    elif view_option == 'Weekly':
-        st.write("Weekly View")
-        st.write("Data from Sep2021 - Dec2021 is zero due to covid.")
-        st.dataframe(data4_weekly.reset_index(drop=True)[['Week', 'Room Revenue', 'Rooms Sold']]) 
-        st.dataframe(data5_weekly.reset_index(drop=True)[['Week', 'Arrival Rooms', 'Individual Confirm', 'Individual Revenue']])
+    # elif view_option == 'Weekly':
+    #     st.write("Weekly View")
+    #     st.write("Data from Sep2021 - Dec2021 is zero due to covid.")
+    #     st.dataframe(data4_weekly.reset_index(drop=True)[['Week', 'Room Revenue', 'Rooms Sold']]) 
+    #     st.dataframe(data5_weekly.reset_index(drop=True)[['Week', 'Arrival Rooms', 'Individual Confirm', 'Individual Revenue']])
        
-    elif view_option == 'Yearly':
-        st.write("Yearly View")
-        st.write("Data from Sep2021 - Dec2021 is zero due to covid.")
-        st.dataframe(data4_yearly.reset_index(drop=True)[['Year', 'Room Revenue','Rooms Sold']])  
-        st.dataframe(data5_yearly.reset_index(drop=True)[['Year', 'Arrival Rooms', 'Individual Confirm', 'Individual Revenue']])
+    # elif view_option == 'Yearly':
+    #     st.write("Yearly View")
+    #     st.write("Data from Sep2021 - Dec2021 is zero due to covid.")
+    #     st.dataframe(data4_yearly.reset_index(drop=True)[['Year', 'Room Revenue','Rooms Sold']])  
+    #     st.dataframe(data5_yearly.reset_index(drop=True)[['Year', 'Arrival Rooms', 'Individual Confirm', 'Individual Revenue']])
 
 if __name__ == '__main__':
     main()
